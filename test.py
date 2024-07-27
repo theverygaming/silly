0c1f985861bee91cdf09f90eaa048a55086a257c
theverygaming <theverygaming@users.noreply.github.com>
2024-07-27 16:48:36 +0200

    fixed pyproject.toml to include modules

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     conn = sillyorm.dbms.sqlite.SQLiteConnection("test.db", check_same_thread=False)
 
     silly.main.init(conn)
-    silly.modload.set_module_paths(["modules/"])
+    silly.modload.set_module_paths(["silly/data/modules/"])
     silly.modload.load_module("webclient", silly.main.env)
     silly.modload.load_module("activitypub", silly.main.env)
     silly.main.run()
