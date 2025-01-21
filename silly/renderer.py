8f74944239c10cc3ee38bf8838443556632c7bca
theverygaming <theverygaming@users.noreply.github.com>
2025-01-21 08:32:35 +0100

    use record for a template

--- silly/renderer.py
+++ silly/renderer.py
@@ -94,6 +94,7 @@ def _render_html(get_template_fn, element, render_ctx, render_self=False):
     def process_attrs(render_tag, render_tail, render_text, render_children):
         output = ""
         for k, v in element.attrib.items():
+            # FIXME: a bunch of these ofc allow injecting HTML (t-att for example), they do gotta be fixed
             # t-att-
             if k.startswith("t-att-"):
                 del element.attrib[k]
