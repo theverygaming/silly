25a22177bbd10e09f1674691b18f049789280dbf
theverygaming <theverygaming@users.noreply.github.com>
2025-01-26 14:43:45 +0100

    upgrade to sillyORM 0.8.1

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@ with import (fetchTarball https://github.com/NixOS/nixpkgs/archive/107d5ef05c0b1
 let
   sillyORMPackage = pkgs.python312Packages.buildPythonPackage rec {
     pname = "sillyorm";
-    version = "0.8.0";
+    version = "0.8.1";
     pyproject = true;
 
     build-system = [
@@ -11,7 +11,7 @@ let
 
     src = fetchPypi {
       inherit pname version;
-      hash = "sha256-P2tPAxW8FgF7DbRk1gZqT45oD4Gh6JTu0SOewqFU8uE=";
+      hash = "sha256-J/k/P178iNjoGApD0kj08uymqlftFXsqGfUoUIHhcIc=";
     };
   };
 in
