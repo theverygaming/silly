7e92002e23598840e9ceb2667d57c0324b11e77c
theverygaming <theverygaming@users.noreply.github.com>
2025-01-31 10:24:24 +0100

    upgrade sillyORM version

--- shell.nix
+++ shell.nix
@@ -18,8 +18,8 @@ let
     */
     src = fetchgit {
       url = "https://github.com/theverygaming/sillyORM.git";
-      rev = "06b63004479fc6411db2a9c7002cc2ba0293968e";
-      hash = "sha256-T40FncjSgEt6d8252zZVrGWW7BG4LNefUy1ZA1ZIokc=";
+      rev = "32a5b121ea1e072b871e0f333a92309f7a3e9ec4";
+      hash = "sha256-awCMipb7hxodO+GAO6/jzl63UyM+fvU1BK+c7OtHQiw=";
     };
   };
 in
