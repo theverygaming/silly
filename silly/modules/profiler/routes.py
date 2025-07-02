52ba182b8799e12432957b703e19c86a4f4b57b3
theverygaming <theverygaming@users.noreply.github.com>
2025-07-02 22:04:57 +0200

    profiler formatting

--- silly/modules/profiler/routes.py
+++ silly/modules/profiler/routes.py
@@ -10,6 +10,7 @@ _profile_data = {}
 
 _profile_action = None
 
+
 def _trace(frame, event, arg):
     if _profile_action not in _profile_data:
         return
@@ -19,13 +20,16 @@ def _trace(frame, event, arg):
         t = "C"
     else:
         return
-    _profile_data[_profile_action]["data"].append((
-        int(time.perf_counter() * 1e9), # timestamp
-        t, # type
-        frame.f_code.co_filename, # filename
-        frame.f_code.co_firstlineno, # line
-        frame.f_code.co_name, # fnname
-    ))
+    _profile_data[_profile_action]["data"].append(
+        (
+            int(time.perf_counter() * 1e9),  # timestamp
+            t,  # type
+            frame.f_code.co_filename,  # filename
+            frame.f_code.co_firstlineno,  # line
+            frame.f_code.co_name,  # fnname
+        )
+    )
+
 
 def _http_profile_start(wrap):
     global _profile_action
@@ -35,10 +39,12 @@ def _http_profile_start(wrap):
     _profile_data[_profile_action]["start"] = int(time.perf_counter() * 1e9)
     sys.setprofile(_trace)
 
+
 def _http_profile_end(wrap):
     sys.setprofile(None)
     if _profile_action:
         _profile_data[_profile_action]["end"] = int(time.perf_counter() * 1e9)
+
 
 class Routes(http.Router):
     @http.route("/debug/profiler/start", with_env=False)
@@ -66,7 +72,7 @@ class Routes(http.Router):
 
             while _profile_data[key]["data"][-1][1] == "O":
                 _profile_data[key]["data"].pop(-1)
-            
+
             for timestamp, t, filename, line, fnname in _profile_data[key]["data"]:
                 k = (filename, line, fnname)
                 if k not in framecache:
@@ -92,6 +98,7 @@ class Routes(http.Router):
                 "exporter": "silly",
                 "name": "silly",
             }
+
         return _get_data_for(list(sorted(_profile_data.keys()))[int(idx)])
 
     @http.route("/debug/profiler/speedscope/<idx>")
@@ -99,9 +106,7 @@ class Routes(http.Router):
         [[x, i] for i, x in enumerate(sorted(_profile_data.keys()))]
         return env["template"].render(
             "profiler.speedscope",
-            {
-                "profile_url": f"/debug/profiler/speedscope/data/{idx}"
-            },
+            {"profile_url": f"/debug/profiler/speedscope/data/{idx}"},
         )
 
     @http.route("/debug/profiler/speedscope/")
@@ -110,6 +115,9 @@ class Routes(http.Router):
             "profiler.speedscope",
             {
                 "profile_url": None,
-                "profiles": [[x, f"/debug/profiler/speedscope/{i}"] for i, x in enumerate(sorted(_profile_data.keys()))],
+                "profiles": [
+                    [x, f"/debug/profiler/speedscope/{i}"]
+                    for i, x in enumerate(sorted(_profile_data.keys()))
+                ],
             },
         )
