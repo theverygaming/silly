1826d0b689b49430b1c4f7e84ac62e2d1ba460c3
theverygaming <theverygaming@users.noreply.github.com>
2025-07-12 19:39:14 +0200

    add reexec functionality to cli, allow loglevel to be set via config

--- silly/cli.py
+++ silly/cli.py
@@ -1,4 +1,6 @@
 import argparse
+import os
+import sys
 import logging
 from . import main, tests, modload, mod
 
@@ -6,26 +8,64 @@ _logger = logging.getLogger(__name__)
 
 
 class SillyConfig:
+    _LOGLEVEL_MAP = {
+        "debug": logging.DEBUG,
+        "info": logging.INFO,
+        "warning": logging.WARNING,
+    }
+
+    _DEFAULTS = {
+        "connstr": None,
+        "module_path": ["modules/"],
+        "loglevel": _LOGLEVEL_MAP["debug"],
+    }
+
     def __init__(self):
-        self.connstr = None
-        self.module_path = ["modules/"]
+        self.connstr = self._DEFAULTS["connstr"]
+        self.module_path = self._DEFAULTS["module_path"]
+        self.loglevel = self._DEFAULTS["loglevel"]
 
     def init_arg_parser(self, parser):
         parser.add_argument("--connstr", type=str, required=True)
         parser.add_argument("--module_path", nargs="+", type=str)
+        parser.add_argument(
+            "--loglevel", type=str, choices=("debug", "info", "warning"), default="info"
+        )
 
     def process_argparser_args(self, namespace):
         self.connstr = namespace.connstr or self.connstr
         self.module_path = namespace.module_path or self.module_path
+        self.loglevel = self._LOGLEVEL_MAP[namespace.loglevel]
         self.process_args()
 
     def process_args(self):
         modload.add_module_paths(self.module_path)
+        logging.basicConfig(
+            format="%(asctime)s %(levelname)s %(name)s: %(message)s", level=logging.INFO
+        )
+        logging.getLogger("silly").setLevel(self.loglevel)
 
 
-def cmd_run(config: SillyConfig):
+def reexec(executable=None, argv=None):
+    if argv is None:
+        argv = sys.argv
+    if executable is None:
+        executable = sys.executable
+
+    if not argv or argv[0] != executable:
+        argv.insert(0, executable)
+    os.execve(executable, argv, os.environ)
+
+
+def cmd_run(config: SillyConfig, do_reexec=True):
     main.init(config.connstr)
-    main.run()
+    try:
+        main.run()
+    except mod.SillyRestartException:
+        if do_reexec:
+            reexec()
+        else:
+            raise
 
 
 def cmd_test(config: SillyConfig):
@@ -54,10 +94,6 @@ def cmd_uninstall(config: SillyConfig, modules: list[str], throw_exc=False):
 
 
 def entry(args):
-    logging.basicConfig(
-        format="%(asctime)s %(levelname)s %(name)s: %(message)s", level=logging.INFO
-    )
-    logging.getLogger("silly").setLevel(logging.DEBUG)
     config = SillyConfig()
     parser = argparse.ArgumentParser(
         description="meow",
