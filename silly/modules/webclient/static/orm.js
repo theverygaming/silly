b4502021428c6b80f30f3d767f48bfd2e46ec390
theverygaming <theverygaming@users.noreply.github.com>
2025-08-11 21:53:27 +0200

    simplify frontend recordset

--- silly/modules/webclient/static/orm.js
+++ silly/modules/webclient/static/orm.js
@@ -14,46 +14,41 @@ function convORMJsonrpcType(env, obj) {
         return vals;
     }
     if (isSillyRecordset(obj)) {
-        const ids = [];
         const fields = [];
         // accumulate ids and field values for the recordset, recurse into all field values and convert them
         for (const rec of obj.records) {
-            ids.push(convORMJsonrpcType(env, rec["id"].value));
-            //delete rec["id"];
             const fields_obj = {};
             for (const [key, value] of Object.entries(rec)) {
                 fields_obj[key] = convORMJsonrpcType(env, value);
             }
             fields.push(fields_obj);
         }
-        return new Recordset(env, obj.model, ids, fields).asProxy();
+        return new Recordset(env, obj.model, fields).asProxy();
     }
     return obj;
 }
 
 class Recordset {
-    constructor(env, model, ids, fields) {
+    constructor(env, model, fields) {
         this.env = env;
         this.model = model;
-        this._ids = ids;
         this.fields = fields;
     }
 
     // Recordset methods
 
     ensureOne() {
-        if (this.ids.length != 1) {
-            throw new Error(`ensureOne found ${this.ids.length} IDs in recordset`);
+        if (this.fields.length != 1) {
+            throw new Error(`ensureOne found ${this.fields.length} IDs in recordset`);
         }
     }
 
     get id() {
-        this.ensureOne();
-        return this.ids[0];
+        return this.getFieldValue("id");
     }
 
     get ids() {
-        return this._ids;
+        return this.getFieldValueMulti("id");
     }
 
     getField(name) {
@@ -61,8 +56,16 @@ class Recordset {
         return this.fields[0][name];
     }
 
+    getFieldMulti(name) {
+        return this.fields.map(f => f[name]);
+    }
+
     getFieldValue(name) {
         return this.getField(name).value;
+    }
+
+    getFieldValueMulti(name) {
+        return this.getFieldMulti(name).map(f => f.value);
     }
 
     asProxy() {
@@ -77,14 +80,14 @@ class Recordset {
     }
 
     getRecordAtIdx(idx) {
-        if (idx >= this.ids.length) {
-            throw new Error(`cannot get record at index ${idx}, there are only ${this.ids.length} records in this recordset`);
+        if (idx >= this.fields.length) {
+            throw new Error(`cannot get record at index ${idx}, there are only ${this.fields.length} records in this recordset`);
         }
-        return new Recordset(this.env, this.model, [this.ids[idx]], [this.fields[idx]]).asProxy();
+        return new Recordset(this.env, this.model, [this.fields[idx]]).asProxy();
     }
 
     getNRecords() {
-        return this.ids.length;
+        return this.fields.length;
     }
 
     [Symbol.iterator]() {
@@ -111,7 +114,7 @@ class Recordset {
             args: args,
             kwargs: kwargs,
         };
-        const has_records = this.ids.length > 0;
+        const has_records = this.fields.length > 0;
         if (has_records) {
             params.ids = this.ids;
         }
@@ -129,7 +132,7 @@ class Environment {
     }
 
     model(name) {
-        return new Recordset(this, name, [], []);
+        return new Recordset(this, name, []);
     }
 
     async lookupXMLId(xmlid, model = null) {
