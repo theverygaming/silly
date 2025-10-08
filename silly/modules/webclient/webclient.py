98796f16fa5c4fb18174a4839d07586a15b4266f
theverygaming <theverygaming@users.noreply.github.com>
2025-10-08 14:14:13 +0200

    stuff:tm:

--- silly/modules/webclient/webclient.py
+++ silly/modules/webclient/webclient.py
@@ -4,34 +4,15 @@ import silly.modload
 import sillyorm
 
 from silly import http
-from silly.modules.jsonrpc.routes import JSONRPCRoutes
 
 
 class WebclientRoutes(http.Router):
     @http.route("/webclient/")
     def webclient(self, request):
-
-        importmap = {"imports": {"@preact": "https://esm.sh/preact@10.26.4"}}
-
-        for file in silly.modload.staticfiles:
-            if not file.startswith("js/"):
-                continue
-            filep = PurePath(file)
-            p = filep.parent.relative_to("js/") / filep.name
-            importmap["imports"][f"@{str(p.with_suffix(''))}"] = "/static/js/" + str(p)
-
         return request.env["core.template"].render_html_resp(
-            "webclient.template_test2",
+            "webclient.index",
             {
-                "importmap": json.dumps(importmap),
+                "mainmenu": False,
             },
         )
         # TODO: error page
-
-
-class InheritJSONRPCRoutes(JSONRPCRoutes):
-    def jsonrpc_conv_type(self, env, val):
-        ret = super().jsonrpc_conv_type(env, val)
-        if isinstance(val, sillyorm.model.Model):
-            ret["spec"] = env[val._name].webclient_model_spec()
-        return ret
